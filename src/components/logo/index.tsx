d5f7ee6262defb5b47cd1dba8ee6ec1b3eb03589
cmtchuong <19520430@gm.uit.edu.vn>
2022-04-04 11:57:29 +0700

    'init'

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { NextjsIcon } from "@components/icons";
+
+export const Logo: React.FC = () => {
+    return (
+            <NextjsIcon   data-test="icon"  className="text-white mx-auto" width="96" height="58" />
+    );
+};
